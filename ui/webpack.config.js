ea8ce85d478db50e559827c931f84b814b517170
mcgeorgiev <michaelcgeorgiev@protonmail.com>
2018-11-26 20:28:55 +0000

    introduce template for webpack

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: [
@@ -24,9 +25,9 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
       }
     ]
@@ -35,35 +36,42 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: __dirname + '/dist',
     publicPath: '/',
+    path: __dirname + '/dist',
     filename: 'bundle.js'
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Portfolio',
+      template: __dirname + '/index.html',
+      inject: 'body'
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-        'process.env': {
-          'SECRET_KEY': JSON.stringify("f_w^fy7oax5&iln&l@a4z@*m3ts584%pbx*fnxd7108$rj84t3")
-        }
+      'process.env': {
+        'SECRET_KEY': JSON.stringify('f_w^fy7oax5&iln&l@a4z@*m3ts584%pbx*fnxd7108$rj84t3')
       }
+    }
     )
   ],
   devServer: {
     contentBase: './dist',
     port: 8080,
     hot: true,
-    historyApiFallback: true,
+    historyApiFallback: {
+      index: 'index.html'
+    },
     proxy: {
       '/api': {
-         target: {
-            host: "127.0.0.1",
-            protocol: 'http:',
-            port: 5000
-         },
-         pathRewrite: {
-            '^/api': ''
-         }
+        target: {
+          host: '127.0.0.1',
+          protocol: 'http:',
+          port: 5000
+        },
+        pathRewrite: {
+          '^/api': ''
+        }
       }
-   }
+    }
   }
-};
+}
